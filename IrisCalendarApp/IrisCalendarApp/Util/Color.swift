64ad628cfa20f984fcfdbc152312be97200883ab
DAEUN28 <acone1128@gmail.com>
2019-09-10 11:32:07 +0900

    Update Color.swift
    Fix Color error

--- IrisCalendarApp/IrisCalendarApp/Util/Color.swift
+++ IrisCalendarApp/IrisCalendarApp/Util/Color.swift
@@ -9,40 +9,36 @@
 import UIKit
 
 struct Color {
-    static let main: UIColor = UIColor(hex: "#7247B2") ?? UIColor.gray
-    static let mainHalfClear: UIColor = UIColor(hex: "#7247B2")?.withAlphaComponent(0.67) ?? UIColor.gray
-    static let authTextfield: UIColor = UIColor(hex: "#A2D7E3") ?? UIColor.gray
-    static let skyblueCategory: UIColor = UIColor(hex: "#3CB8EF") ?? UIColor.gray
-    static let pinkCategory: UIColor = UIColor(hex: "#D92D73") ?? UIColor.gray
-    static let yelloCategory: UIColor = UIColor(hex: "#FDA921") ?? UIColor.gray
-    static let lightGray: UIColor = UIColor(hex: "#D5D5D5") ?? UIColor.gray
-    static let defaultGray: UIColor = UIColor(hex: "#B4B4B4") ?? UIColor.gray
+    static let main = UIColor().hexStringToUIColor(hex: "#7247B2")
+    static let mainHalfClear = UIColor().hexStringToUIColor(hex: "#7247B2").withAlphaComponent(0.67)
+    static let authTxtField = UIColor().hexStringToUIColor(hex: "#A2D7E3")
+    static let skyblueCategory = UIColor().hexStringToUIColor(hex: "#3CB8EF")
+    static let pinkCategory = UIColor().hexStringToUIColor(hex: "#D92D73")
+    static let yelloCategory = UIColor().hexStringToUIColor(hex: "#FDA921")
+    static let lightGray = UIColor().hexStringToUIColor(hex: "#D5D5D5")
+    static let defaultGray = UIColor().hexStringToUIColor(hex: "#B4B4B4")
 }
 
 extension UIColor {
-    public convenience init?(hex: String) {
-        let r, g, b, a: CGFloat
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
         
-        if hex.hasPrefix("#") {
-            let start = hex.index(hex.startIndex, offsetBy: 1)
-            let hexColor = String(hex[start...])
-            
-            if hexColor.count == 8 {
-                let scanner = Scanner(string: hexColor)
-                var hexNumber: UInt64 = 0
-                
-                if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = CGFloat(hexNumber & 0x000000ff) / 255
-                    
-                    self.init(red: r, green: g, blue: b, alpha: a)
-                    return
-                }
-            }
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
         }
         
-        return nil
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
 }
