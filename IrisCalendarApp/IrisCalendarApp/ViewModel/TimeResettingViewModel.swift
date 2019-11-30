ede3aac318cf9810d67348d14c14cad931c07704
DAEUN28 <acone1128@gmail.com>
2019-11-30 14:19:48 +0900

    Update Auth
    Auth 파트 리팩토링 완료

--- IrisCalendarApp/IrisCalendarApp/ViewModel/TimeResettingViewModel.swift
+++ IrisCalendarApp/IrisCalendarApp/ViewModel/TimeResettingViewModel.swift
@@ -12,65 +12,38 @@ import RxSwift
 import RxCocoa
 
 class TimeResettingViewModel: ViewModelType {
-
     private let disposeBag = DisposeBag()
 
     struct Input {
-        let timeSettingTaps: Signal<Void>
-        let viewDidLoad: Signal<Void>
-        let startTimeTaps: Signal<Void>
-        let endTimeTaps: Signal<Void>
+        let loadData: Completable
         let selectedTime: Driver<Date>
+        let startTimeTap: Signal<Void>
+        let endTimeTap: Signal<Void>
+        let doneTap: Signal<Void>
     }
 
     struct Output {
         let startTime: Driver<String>
         let endTime: Driver<String>
         let isEnabled: Driver<Bool>
-        let result: Driver<String>
+        let result: Signal<String>
     }
 
     func transform(input: TimeResettingViewModel.Input) -> TimeResettingViewModel.Output {
         let api = AllocationTimeAPI()
-        let allocationTimeFormat = "HH:mm"
-        let startTime = BehaviorRelay<String>(value: allocationTimeFormat)
-        let endTime = BehaviorRelay<String>(value: allocationTimeFormat)
+        let startTime = BehaviorRelay<String>(value: IrisDateFormat.time.rawValue)
+        let endTime = BehaviorRelay<String>(value: IrisDateFormat.time.rawValue)
         let result = PublishSubject<String>()
         let info = Driver.combineLatest(startTime.asDriver(), endTime.asDriver())
+        let isEnabled = info.map { IrisDateFormat.time.isStartFaster(startTime: $0, endTime: $1) }
 
-        let formatter = DateFormatter()
-        formatter.dateFormat = allocationTimeFormat
-
-        let isEnabled = info.map { (start, end) -> Bool in
-            if start == allocationTimeFormat || end == allocationTimeFormat { return false }
-            if start.components(separatedBy: [":"]).joined() < end.components(separatedBy: [":"]).joined() {
-                return true
-            }
-            result.onNext("종료시간이 더 빠릅니다")
-            return false
-        }.asDriver(onErrorJustReturn: false)
-
-        input.timeSettingTaps.withLatestFrom(info).asObservable().subscribe({ [weak self] (event) in
-            guard let time = event.element, let strongSelf = self else { return }
-            api.updateAlloctionTime(startTime: time.0, endTime: time.1).asObservable().subscribe { (event) in
-                switch event.element {
-                case .ok: result.onCompleted()
-                case .badRequest: result.onNext("유효하지 않은 요청")
-                case .unauthorized: result.onNext("유효하지 않은 토큰")
-                case .serverError: result.onNext("서버오류")
-                default: result.onNext("")
-                }
-            }.disposed(by: strongSelf.disposeBag)
-        }).disposed(by: disposeBag)
-
-        input.viewDidLoad.asObservable().subscribe { [weak self] (_) in
+        input.loadData.subscribe { [weak self] (_) in
             guard let strongSelf = self else { return }
-            api.getAlloctionTime().asObservable().subscribe (onNext: { (response, networkingResult) in
+            api.getAlloctionTime().subscribe(onNext: { (response, networkingResult) in
                 switch networkingResult {
                 case .ok:
                     startTime.accept(response!.startTime)
                     endTime.accept(response!.endTime)
-                    result.onNext("불러오기 성공")
                 case .noContent: result.onNext("잘못된 접근")
                 case .badRequest: result.onNext("유효하지 않은 요청")
                 case .unauthorized: result.onNext("유효하지 않은 토큰")
@@ -80,21 +53,30 @@ class TimeResettingViewModel: ViewModelType {
             }).disposed(by: strongSelf.disposeBag)
         }.disposed(by: disposeBag)
 
-        input.startTimeTaps.withLatestFrom(input.selectedTime).asObservable()
-            .subscribe { (event) in
-                guard let time = event.element else { return }
-                startTime.accept(formatter.string(from: time))
-        }.disposed(by: disposeBag)
+        input.startTimeTap.withLatestFrom(input.selectedTime).asObservable().subscribe(onNext: {
+            startTime.accept(IrisDateFormat.time.toString(date: $0))
+        }).disposed(by: disposeBag)
 
-        input.endTimeTaps.withLatestFrom(input.selectedTime).asObservable()
-            .subscribe { (event) in
-                guard let time = event.element else { return }
-                endTime.accept(formatter.string(from: time))
-        }.disposed(by: disposeBag)
+        input.endTimeTap.withLatestFrom(input.selectedTime).asObservable().subscribe(onNext: {
+            endTime.accept(IrisDateFormat.time.toString(date: $0))
+        }).disposed(by: disposeBag)
+
+        input.doneTap.withLatestFrom(info).asObservable().subscribe(onNext: { [weak self] (start, end) in
+            guard let strongSelf = self else { return }
+            api.updateAlloctionTime(startTime: start, endTime: end).subscribe(onNext: { (response) in
+                switch response {
+                case .ok: result.onCompleted()
+                case .badRequest: result.onNext("유효하지 않은 요청")
+                case .unauthorized: result.onNext("유효하지 않은 토큰")
+                case .serverError: result.onNext("서버오류")
+                default: result.onNext("할당시간 재설정 실패")
+                }
+            }).disposed(by: strongSelf.disposeBag)
+        }).disposed(by: disposeBag)
 
         return Output(startTime: startTime.asDriver(),
                       endTime: endTime.asDriver(),
                       isEnabled: isEnabled,
-                      result: result.asDriver(onErrorJustReturn: "할당시간 재설정 실패"))
+                      result: result.asSignal(onErrorJustReturn: "할당시간 재설정 실패"))
     }
 }
